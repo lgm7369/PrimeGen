8e722c572fc236058a889bf0570c6ce240468f7b
Logan McHerron <lgm7369@rit.edu>
2020-03-06 16:46:34 -0500

    Finished

--- CSCIProject2/PrimeGen.cs
+++ CSCIProject2/PrimeGen.cs
@@ -10,13 +10,20 @@ using System.Threading;
 
 namespace PrimeGen
 {
+    //This program prints out prime numbers that get generated using the Crptography and Parallel classes
+    //It requires an integer arguement for the number of bits that the prime number should be as well as
+    //a count for the total number to print.
     static class PrimeGen
     {
-        static int found = 0;
+        static Found fnd = new Found();
         static int count = 1;
         static Boolean done = false;
         static RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
         static int byteCount = 0;
+
+        //Main function that reads in the arguments and outputs an error message if anything is incorrect
+        //Uses the Parallel class to generate threads that create and check if numbers are prime
+        //Use the stopwatch to record the total time it took to print all the numbers
         static void Main(string[] args)
         {
             var Error = "Invalid arguments. \n" +
@@ -24,7 +31,7 @@ namespace PrimeGen
                         "- bits - the number of bits of the prime number, this must be a \n" +
                         "multiple of 8, and at least 32 bits.\n" +
                         " - count - the number of prime numbers to generate, defaults to 1";
-          
+
             try
             {
                 if (Int32.Parse(args[0]) < 32 || Int32.Parse(args[0]) % 8 != 0)
@@ -39,6 +46,12 @@ namespace PrimeGen
                 Console.WriteLine(Error);
                 Environment.Exit(0);
             }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine(Error);
+                Environment.Exit(0);
+            }
+
             try
             {
                 if (args.Length > 1)
@@ -57,6 +70,8 @@ namespace PrimeGen
                 Environment.Exit(0);
             }
 
+            Console.WriteLine(String.Format("BitLength: {0} bits", byteCount));
+
             Stopwatch watch = new Stopwatch();
             watch.Start();
             Parallel.ForEach(LoopTillFalse(), (Action) =>
@@ -72,11 +87,14 @@ namespace PrimeGen
 
         }
 
+        //This is used in the Parallel for loop to continue spawning threads until the total count has been found
         static IEnumerable<bool> LoopTillFalse()
         {
             while (!done) yield return true;
         }
 
+        //Generates a number with the same bit count as the number specified in the arguments
+        //checks if the generated number is prime. If it is then it outputs the number and adds one to the found count
         static void FindPrime()
         {
             Byte[] num = new byte[byteCount];
@@ -84,17 +102,21 @@ namespace PrimeGen
             var result = new BigInteger(num);
             if (result.IsProbablyPrime() & !done)
             {
-                if (found >= count)
+                if (fnd.found >= count)
                 {
                     done = true;
                 }
                 if (!done)
                 {
-                    Console.WriteLine(String.Format("{0}: {1}", Interlocked.Increment(ref found), result));                 
+                    lock(fnd)
+                    {
+                        Console.WriteLine(String.Format("{0}: {1}", ++fnd.found, result));
+                    }
                 }
             }
         }
 
+        //used to check if the given number is prime
         static Boolean IsProbablyPrime(this BigInteger value, int witnesses = 10)
         {
             if (value <= 1) return false;
@@ -130,5 +152,12 @@ namespace PrimeGen
             return true;
         }
 
+    }
+
+    //This class is used so that the found variable can be locked
+    //Used to make sure that the output is always outputting the numbers in order.
+    class Found
+    {
+        public int found { get; set; }
     }
 }
